bf0e3ec12f391eb260c9e1cc3d34170b57df8855
nediakhelifi <khelifinedia91@gmail.com>
2021-06-15 10:58:04 +0100

    the first part of tamplate

---
+++ thesis/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { SectionsModule } from './sections/sections.module';
+import { ElementsModule } from './elements/elements.module';
+import { ExamplesModule } from './examples/examples.module';
+
+import { AppComponent } from './app.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
+import { PresentationModule } from './presentation/presentation.module';
+import { LandingComponent } from './views/landing/landing.component';
+import { LoginComponent } from './views/login/login.component';
+import { SignupComponent } from './views/signup/signup-u.component';
+import { CompanyComponent } from './views/company/company.component';
+import { SearchcComponent } from './views/searchc/searchc.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        LandingComponent,
+        LoginComponent,
+        SignupComponent,
+        CompanyComponent,
+        SearchcComponent
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        NgbModule,
+        FormsModule,
+        RouterModule,
+        AppRoutingModule,
+        PresentationModule,
+        SectionsModule,
+        ElementsModule,
+        ExamplesModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
