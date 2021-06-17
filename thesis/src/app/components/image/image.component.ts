464a305ef9bce44905941af22987a3c5c70e319a
BSsAmal <bsassiamal@gmail.com>
2021-06-17 21:34:25 +0100

    image component

--- thesis/src/app/components/image/image.component.ts
+++ thesis/src/app/components/image/image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-image',
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image.component.css']
 })
 export class ImageComponent implements OnInit {
+  @Input() isRound: boolean = false;
+  @Input() image: string;
+  state: any = {}
+  @ViewChild('input') input: ElementRef;
+  constructor() { 
+  this.handleImageChange = this.handleImageChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+  this.handleClick = this.handleClick.bind(this);
+  this.handleRemove = this.handleRemove.bind(this);
+}
 
-  constructor() { }
 
-  ngOnInit(): void {
+ngOnInit() {
+  this.state = {
+      file: null,
+      imagePreviewUrl: this.image !== undefined ? this.image:(this.isRound ? 'assets/img/placeholder.jpg':'assets/img/image_placeholder.jpg')
   }
-
+}
+ngAfterViewInit(){
+  this.input.nativeElement.children[2].onchange = this.handleImageChange;;
+}
+handleImageChange(e){
+  e.preventDefault();
+  let reader = new FileReader();
+  let file = e.target.files[0];
+  reader.onloadend = () => {
+      this.state.file = file;
+      this.state.imagePreviewUrl = reader.result;
+      // this.state.imagePreviewUrl1 = reader.result;
+  }
+  reader.readAsDataURL(file);
+}
+handleSubmit(e){
+  e.preventDefault();
+  // this.state.file is the file/image uploaded
+  // in this function you can save the image (this.state.file) on form submit
+  // you have to call it yourself
+}
+handleClick(){
+  this.input.nativeElement.children[2].click();
+}
+handleRemove(){
+  this.state.file = null;
+  this.state.imagePreviewUrl = this.image !== undefined ? this.image:(this.isRound ? 'assets/img/placeholder.jpg':'assets/img/image_placeholder.jpg');
+}
 }
